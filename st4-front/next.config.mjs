6a7ca093b66e7d070627b5f87eb56640dcbbcbd8
Mateusz "Mattu" Partyka <mateusz.pl671@gmail.com>
2024-05-23 13:04:52 +0200

    Update next.config.mjs

--- st4-front/next.config.mjs
+++ st4-front/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/st4",
+  reactStrictMode: true,
+  trailingSlash: true,
+};
 
 export default nextConfig;
