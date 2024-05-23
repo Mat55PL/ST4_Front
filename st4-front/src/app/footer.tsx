fc24ad2bfee3411c03a76498ffcddd7612ad92dd
Mateusz "Mattu" Partyka <mateusz.pl671@gmail.com>
2024-05-23 12:55:51 +0200

    Update footer.tsx

--- st4-front/src/app/footer.tsx
+++ st4-front/src/app/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="bg-blue-500 p-6 text-center text-white">
-            <p className="mb-2">© 2024 Autorzy: Mateusz Partyka, Paweł Sołtys, Gabriela Nowak, Jakub Łyszczek</p>
+            <p className="mb-2">2024 Autorzy: Mateusz Partyka, Paweł Sołtys, Gabriela Nowak, Jakub Łyszczek</p>
             <p>Projekt zespołowy na przedmiot "Szkolenie Techniczne 4"</p>
         </footer>
     )
